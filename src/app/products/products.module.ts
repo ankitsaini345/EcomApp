afe30a45fe31bceafb948383c65d2a1241e3a1b1
ankitsaini345 <ankitsaini345@gmail.com>
2020-02-12 12:36:56 +0530

    product module added (temp)

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import { ProductDetailComponent } from ''
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ProductDetailComponent
+  ]
+})
+export class ProductsModule { }
