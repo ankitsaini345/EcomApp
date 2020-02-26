bc0fc4560c4f25b2accea98baf2463c39aff0876
ankitsaini345 <ankitsaini345@gmail.com>
2020-02-26 13:59:31 +0530

    password matcher added

--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,7 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
-
+function checkLength(val: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (!c.value)
+      return null;
+    if (c.value.toString().length == val)
+      return null;
+    return { length: true };
+  };
+}
+function PaswdMatch(c: AbstractControl): { [key: string]: boolean } | null {
+  let pswd = c.get('password');
+  let cpswd = c.get('cPassword');
+  if (pswd.pristine || cpswd.pristine)
+    return null;
+  if (pswd.value === cpswd.value)
+    return null;
+  return { match: true };
+}
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,31 +26,42 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   signUpForm: FormGroup;  //Root Form group
-
-  firstName= ''
-  lastName = ''
-  email = ''
-  mobile = ''
-  password = ''
-  cPassword = ''
-
-  constructor() { }
+  mobilePtrn = '^[0-9]*$';
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.signUpForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName : new FormControl(),
-      email : new FormControl(),
-      mobile : new FormControl(),
-      password : new FormControl(),
-      cPassword : new FormControl()
+
+    this.signUpForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(5)]],
+      lastName: [{ value: 'NA', disabled: true }, [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.email, Validators.required]],
+      mobile: ['', [checkLength(10),Validators.pattern(this.mobilePtrn)]],
+      paswrdGroup: this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        cPassword: ['', [Validators.required]]
+      },
+        { validator: PaswdMatch }
+      ),
+      notification: 'email'
     });
-    this.signUpForm.patchValue({
-      firstName : 'testName'
-    });
+
+    // this.signUpForm.patchValue({
+    //   firstName: 'testName'
+    // });
   }
   save() {
     console.log(this.signUpForm.value);
   }
-  
+
+  setNotifyVia(val: string): void {
+    const phoneControl = this.signUpForm.get('mobile');
+    if (val == 'email') {
+      phoneControl.clearValidators();
+    }
+    else {
+      phoneControl.setValidators([Validators.required])
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
 }
